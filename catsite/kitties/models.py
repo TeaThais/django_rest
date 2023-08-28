ee24f2b0d74b723eec73b008edc9f9d5dc3a9fbe
Tais <72388962+TeaThais@users.noreply.github.com>
2023-08-28 23:44:50 +0200

    basic_class_APIView

--- catsite/kitties/models.py
+++ catsite/kitties/models.py
@@ -3,13 +3,11 @@ from django.db import models
 
 class Kitties(models.Model):
     title = models.CharField(max_length=225, verbose_name='Name')
-    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name='URL')
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos")
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
-    cat = models.ForeignKey('Category', on_delete=models.PROTECT)
+    cat = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
--- catsite/kitties/views.py
+++ catsite/kitties/views.py
@@ -1,9 +1,27 @@
+from django.forms import model_to_dict
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 from .models import Kitties
 from .serializers import KittiesSerializer
 
 
-class KittiesAPIView(generics.ListAPIView):
-    queryset = Kitties.objects.all()
-    serializer_class = KittiesSerializer
+class KittiesAPIView(APIView):
+    def get(self, request):
+        lst = Kitties.objects.all().values()
+        return Response({'posts': list(lst)})
+
+    def post(self, request):
+        new_post = Kitties.objects.create(
+            title=request.data['title'],
+            content=request.data['content'],
+            cat_id=request.data['cat_id']
+        )
+        return Response({'post': model_to_dict(new_post)})
+
+
+# class KittiesAPIView(generics.ListAPIView):
+#     queryset = Kitties.objects.all()
+#     serializer_class = KittiesSerializer
